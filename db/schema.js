d38066422e13638217fe6270f3ead76bddb73fdb
ooeid <ooeid.omar@gmail.com>
2020-01-19 18:14:21 +0300

    part1

--- db/schema.js
+++ db/schema.js
@@ -1,4 +1,32 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/yum');
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
+db.on('error', console.error.bind(console, 'connection error:')); 
+
+let Schema = mongoose.Schema 
+
+let Menu = new Schema ({
+    title: String
+})
+
+let Restaurant = new Schema ({
+name: String,
+yelpUrl: String,
+address:
+        {
+        street: String,
+        zipcode: Number,
+        },
+items: [Menu]
+})
+
+let RestaurantModel = mongoose.model("dinner",Menu)
+let MenuModel = mongoose.model("Kabab",Restaurant)
+
+
+module.exports = {
+    RestaurantModel: RestaurantModel,
+    MenuModel: MenuModel
+}
+
+
--- db/seed.js
+++ db/seed.js
@@ -1,1 +1,59 @@
+
 let Schema = require("../db/schema.js");
+let RestaurantModel = Schema.RestaurantModel
+let MenuModel = Schema.MenuModel
+
+let dinner = new MenuModel({
+    title: "dinner"
+})
+
+let lunch = new MenuModel({
+    title: "lunch"
+})
+
+let breakfast = new MenuModel({
+    title: "breakfast"
+})
+
+
+
+let menu =[dinner,lunch,breakfast]
+
+
+
+
+
+let kabab = new RestaurantModel({
+    name:"kabab",
+    yelpUrl:"don't know what the hell doesn't that mean",
+    address:
+    { street:"freedom street, exit 5", zipcode: 55555},
+    items:menu
+})
+
+let kabab1 = new RestaurantModel({
+    name:"kabab1",
+    yelpUrl:"don't know what the hell doesn't that mean",
+    address:
+    { street:"freedom street, exit 5", zipcode: 55555},
+    items:menu
+})
+
+let kabab2 = new RestaurantModel({
+    name:"kabab2",
+    yelpUrl:"don't know what the hell doesn't that mean",
+    address:
+    { street:"freedom street, exit 5", zipcode: 55555},
+    items:menu
+})
+
+let kabab3 = new RestaurantModel({
+    name:"kabab3",
+    yelpUrl:"don't know what the hell doesn't that mean",
+    address:
+    { street:"freedom street, exit 5", zipcode: 55555},
+    items:menu
+})
+
+
+

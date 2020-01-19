22b563e610b07c4510b57743d1488cc8781c5833
W2AlharbiMe <w2.alharbi.me@Gmail.com>
2020-01-19 17:40:36 +0300

    finished first two

--- db/schema.js
+++ db/schema.js
@@ -1,4 +1,34 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/yum');
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/yum", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
+
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => console.log("database successfully connected"));
+
+const Schema = mongoose.Schema;
+
+const menuItemSchema = new Schema({
+  title: String
+});
+
+const restaurantSchema = new Schema({
+  name: String,
+  address: {
+    street: String,
+    zipcode: Number
+  },
+  yelpUrl: String,
+  items: [menuItemSchema]
+});
+
+const Restaurant = mongoose.model("Restaurant", restaurantSchema);
+const MenuItem = mongoose.model("MenuItem", menuItemSchema);
+
+module.exports = {
+  Restaurant,
+  MenuItem,
+  db
+};
--- db/seed.js
+++ db/seed.js
@@ -1,1 +1,18 @@
-let Schema = require("../db/schema.js");
+let { Restaurant, MenuItem, db } = require("./schema.js");
+
+const restaurant2 = new Restaurant({
+  name: "Dunkkin",
+  address: { street: "Al Faisal", zipcode: 8174961 },
+  yelpUrl: "https://www.yelp.com/"
+});
+const menuItem2 = new MenuItem({ title: "Chocolate Donats" });
+const menuItem3 = new MenuItem({ title: "Coffee" });
+
+restaurant2.items.push(menuItem3);
+restaurant2.items.push(menuItem2);
+
+restaurant2
+  .save()
+  .then(r => console.log(r))
+  .then(() => db.close())
+  .catch(err => console.log(err));

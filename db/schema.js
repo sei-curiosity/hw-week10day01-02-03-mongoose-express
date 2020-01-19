f791cd249a257a8fae72a5ce3c9d1facf1da3241
roush55 <ranaabumelha@gmail.com>
2020-01-19 20:08:25 +0300

    hw-week10-day01

--- db/schema.js
+++ db/schema.js
@@ -1,4 +1,20 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/yum');
+mongoose.connect('mongodb://localhost/yum',{ useNewUrlParser: true ,useUnifiedTopology: true,useFindAndModify: false, useCreateIndex: true});
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
+
+let Schema = mongoose.Schema;
+
+let MenuItemSchema=new Schema({
+    title :String
+});
+
+let RestaurantScheme=new Schema({
+    name:String,
+    address:{street:String,zipcode:Number},
+    yelpUrl:String,
+    items:[MenuItemSchema]
+});
+
+let MenuItemModel=mongoose.model('MenuItem', MenuItemSchema);
+let RestaurantModel=mongoose.model('Restaurant', RestaurantScheme);

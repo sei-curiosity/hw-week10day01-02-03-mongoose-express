87ed3b41c6ebc47d62ce2f8949876c9724823820
alsubi <alsubaie22145@gmail.com>
2020-01-19 17:55:13 +0300

    hw w10-d1 part1-s1-2

--- db/schema.js
+++ db/schema.js
@@ -1,4 +1,32 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/yum');
+mongoose.connect('mongodb://localhost/yum',
+{useNewUrlParser: true , useUnifiedTopology: true});
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open',function(){
+    console.log("database successfully cunnected")
+})
+
+// db.close()
+
+let Schema =mongoose.Schema
+
+let MenuItemsSchema =new Schema({
+    title:String
+})
+
+let RestaurantSchema = new Schema({
+    name: String,
+    address:{street:String,zipcode:Number},
+    yelpUrl:String,
+    items :[MenuItemsSchema]
+})
+
+let RestaurantModel = mongoose.model("Restaurant",RestaurantSchema)
+let MenuItemsModle = mongoose.model("MenuItem",MenuItemsSchema)
+
+module.exports={
+    RestaurantModel : RestaurantModel,
+    MenuItemsModle : MenuItemsModle
+}
--- db/seed.js
+++ db/seed.js
@@ -1,1 +1,42 @@
-let Schema = require("../db/schema.js");
+// let Schema = require("../db/schema.js");
+let Schema =require("./schema")
+
+let Restaurant =Schema.RestaurantModel
+let MenuItem = Schema.MenuItemsModle
+
+Restaurant.remove()
+.then(()=>{
+    console.log(" All Restaurant deleted ")
+})
+.catch(error=>{
+
+    console.log(error)
+})
+
+
+MenuItem.remove()
+.then(()=>{
+    console.log(" All MenuItem deleted ")
+})
+.catch(error=>{
+    console.log(error)
+})
+
+let item1 = new MenuItem({title:"coffee"})
+let item2 =new MenuItem({title:"donuts"})
+let item3 =new MenuItem({title:"pizza"})
+
+let items=[item1,item2,item3]
+
+let restaurant1 = new Restaurant({name:"DD",address:{street:"street103",zipcode:44},yelpUrl:"itsUrl/dd",items:items})
+let restaurant2 = new Restaurant({name:"cafeteria",address:{street:"street209",zipcode:22},yelpUrl:"itsUrl/cafeteria",items:items})
+
+let restaurants=[restaurant1,restaurant2]
+
+Restaurant.insertMany(restaurants)
+.then(()=>{
+    console.log(`Add resturant ${restaurants}`)
+})
+.catch(error=>{
+    console.log(error)
+})

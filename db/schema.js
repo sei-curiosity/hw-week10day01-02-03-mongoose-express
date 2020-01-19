ebe75a7e22210a73485d1d5379a0427421a88a94
mohammedshatry <mohammedshatry@hotmail.com>
2020-01-19 22:58:17 +0300

    HW-mongodb

--- db/schema.js
+++ db/schema.js
@@ -2,3 +2,25 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/yum');
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open',function(){
+    console.log("Database is Connected")
+})
+
+let Schema =mongoose.Schema
+
+let MenuItemsSchema =new Schema({
+    title:String
+})
+
+let RestaurantSchema = new Schema({
+    name:String,
+    address:{street:String,zipcode:Number},
+    yelpUrl:String,
+    items :[MenuItemsSchema]
+})
+
+module.exports={
+    RestaurantModel : RestaurantModel,
+    MenuItemsModle : MenuItemsModle
+} 

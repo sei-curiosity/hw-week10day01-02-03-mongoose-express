ccf61f35f55248a4ca9b2c3eb504f3f166eb2532
monano <mona.gaihab@gmail.com>
2020-01-19 21:11:39 +0300

    hw-week10day01-solved

--- db/schema.js
+++ db/schema.js
@@ -2,3 +2,21 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/yum');
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
+let Schema = mongoose.Schema
+let MenuSchema = new Schema({
+    title: String
+})
+let RestaurantSchema = new Schema({
+    name: String,
+    address: {street: String, zipcode: Number},
+    yelpUrl: String,
+    items: [MenuSchema]
+})
+let MenuModel = mongoose.model("Menu",MenuSchema)
+let RestaurantModel = mongoose.model("Restaurant",RestaurantSchema)
+
+module.exports = {
+    RestaurantModel: RestaurantModel,
+    MenuModel: MenuModel
+}
+
--- db/seed.js
+++ db/seed.js
@@ -1,1 +1,47 @@
 let Schema = require("../db/schema.js");
+
+let RestaurantModel = Schema.RestaurantModel
+let MenuModel = Schema.MenuModel
+
+const menu1 = new MenuModel({title:"Pizza"})
+const menu2 = new MenuModel({title:"Omelet"})
+const menu3 = new MenuModel({title:"Shawerma"})
+const menu4 = new MenuModel({title:"Burger"})
+
+
+const restaurant1 = new RestaurantModel({
+    name:"Sahari",
+    address:{
+        street:"Badea Azaman",
+        zipcode:3052
+    },
+    yelpUrl:"https://yelp/sahari.com",
+    items:[menu2,menu3]})
+
+const restaurant2 = new RestaurantModel({
+    name:"Shawerma Plus",
+    address:{
+        street:"Alabraj",
+        zipcode:3072
+    },
+    yelpUrl:"https://yelp/Shawerma.com",
+    items:[menu1,menu2,menu3]})
+
+const restaurant3 = new RestaurantModel({
+    name:"Almadinah",
+    address:{
+        street:"Badea Azaman",
+        zipcode:3056
+    },
+    yelpUrl:"https://yelp/Almadinah.com",
+    items:[menu1,menu2,menu4]})
+
+    const restaurants = [restaurant1,restaurant2,restaurant3]
+
+    RestaurantModel.insertMany(restaurants)
+    .then(()=>{
+        console.log(`Added ${restaurants.length} restaurants to database.`)
+    })
+    .catch((error)=>{
+        console.log(error)
+    })

28c39e3b2c95077710bdb72cc0ec8b92cf83bed1
Modhi Aldughaiyem <modhi1993@gmail.com>
2020-01-19 21:45:01 +0300

    part one 1,2,3

--- db/schema.js
+++ db/schema.js
@@ -2,3 +2,28 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/yum');
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    console.log("database successfully connected")
+})
+
+let Schema = mongoose.Schema
+let MenuItems = new Schema ({
+    title: String
+})
+
+let RestaurantSchema = new Schema ({
+    name: String,
+    address: {street:String,zipcode:Number},
+    yelpUrl: String,
+    items: [MenuItems]
+})
+let MenuModel = mongoose.model("Menu", MenuItems)
+let RestaurantModel =mongoose.model("Restaurant", RestaurantSchema)
+let SushiArt = new RestaurantModel({ name: "SushiArt"});
+SushiArt.save()
+  .then((sushi) => {
+    console.log(sushi);
+  })
+  .catch((error) => {
+    console.log(error)
+  })

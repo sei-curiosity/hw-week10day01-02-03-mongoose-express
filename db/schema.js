b6cdbb23ed13cddcfd7ad1e6d36353041d3feebf
zanaalma <zanaalma1@gmail.com>
2020-01-19 21:48:42 +0300

    hw

--- db/schema.js
+++ db/schema.js
@@ -1,4 +1,25 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/yum');
+mongoose.connect('mongodb://localhost/yum',
+{useNewUrlParser: true , useUnifiedTopology: true } );
+
+
+
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
+db.on('error', 
+console.error.bind(console, 'connection error:'));
+
+let RestaurantSchema = new mongoose.Schema ({
+    name: String,
+    address =[{street: String},{zipcode:Number}],
+    yelpUrl: String,
+    items:[MenuItemsSchema]
+})
+
+let MenuItemsSchema = new mongoose.Schema ({
+    title: String
+
+
+
+})
+let NewRestaurantModel = mongoose.model("Restaurant", RestaurantSchema)
+

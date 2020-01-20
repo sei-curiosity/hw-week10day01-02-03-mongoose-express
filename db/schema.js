f8d1eba3d6557e1414ec4981ad97dc9c497faa99
leenahabi <leenaalnahabi@gmail.com>
2020-01-20 08:55:32 +0300

    wk10d1hw

--- db/schema.js
+++ db/schema.js
@@ -2,3 +2,37 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/yum');
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
+
+
+let Schema = mongoose.Schema
+
+let MenuItems = new Schema ({
+    title: String
+})
+
+let Restaurant = new Schema ({
+    name: String,
+    adress: {street:String,zipcode:Number},
+    yelpUrl:String,
+    items:[MenuItems]
+})
+
+
+let RestaurantModel = mongoose.model("Restuarant",Restaurant)
+let MenuModel = mongoose.model("Menu",MenuItems)
+
+module.exports = {
+    RestaurantModel: RestaurantModel,
+    MenuModel: MenuModel
+}
+// let pizza = new MenuModel({title:"Pizza"});
+// let Yumm = RestaurantModel.create({name: "Yumm", 
+//                         adress: {street:"Waha street",zipcode:11655},
+//                         yelpUrl:"https://www.yelp.com/biz/monza-pizzeria-san-francisco?osq=delivery"})
+//                         Yumm.items.push(pizza)
+// .then((Restuarant) => {
+//     console.log(Restuarant);
+//   })
+//   .catch((error) => {
+//     console.log(error)
+//   })

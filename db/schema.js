f89223d5c72898815881ba5216258cf8fc8d5d4e
gneral20 <gneral20@gmail.com>
2020-01-20 17:45:31 +0300

    done my part

--- db/schema.js
+++ db/schema.js
@@ -2,3 +2,25 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/yum');
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
+
+let Schema = mongoose.Schema
+
+let menuItemSchema = new Schema({
+    title: String
+})
+
+let restaurantSchema = new Schema({
+    name: String,
+    address: {street: String, zipcode: Number},
+    yelpUrl: String,
+    items: []
+})
+
+
+let resturantModel = mongoose.model("Resturant", restaurantSchema)
+let menuItemModel = mongoose.model("MenuItem", menuItemSchema)
+
+module.exports = {
+    resturantModel: resturantModel,
+    menuItemModel: menuItemModel
+}
--- db/seed.js
+++ db/seed.js
@@ -1,1 +1,21 @@
-let Schema = require("../db/schema.js");
+let Schema = require("./db/schema.js");
+let mongoose = require('mongoose');
+
+let resturantModel = Schema.resturantModel;
+let menuItemModel = Schema.menuItemModel;
+
+
+let Menu1 = new menuItemModel({title: "Kabsa"})
+let Menu2 = new menuItemModel({title: "Shawrma"})
+let Menu3 = new menuItemModel({title: "Nodels"})
+
+let menu = [Menu1, Menu2, Menu3]
+
+let MeshalResturant = new resturantModel({
+    name: "Meshal Resturant",
+    address: {street: "al-mothalth", zipcode: 1121},
+    yelpUrl: "//https:www.google.com",
+    items: menu
+})
+
+MeshalResturant.find({name: "Meshal Resturant"})

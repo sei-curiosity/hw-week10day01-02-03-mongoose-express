a83740605043c82c35869d9eba997e84215dec64
Bayan <bayanalbunayan@gmail.com>
2020-01-21 01:59:16 +0300

    p1steps

--- db/schema.js
+++ db/schema.js
@@ -7,16 +7,17 @@ const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
 
-db.once('open', function() {
-    console.log('database successfully connected')
-  })
 
-  let Schema = mongoose.Schema // this is a schema method
-  let menueItem = new Schema ({
+  const done = function(){
+    console.log("connected to DB")
+    db.close()
+}
+
+  let menueItem = new mongoose.Schema ({
     title: String,
   })
 
-let resturantSchema = new Schema ({
+let resturantSchema = new mongoose.Schema ({
     name: String,
     yelpUrl: String,
     items:[menueItem],
@@ -28,51 +29,75 @@ let resturantSchema = new Schema ({
 let restrentModel = mongoose.model("restrunt",resturantSchema )
 let menuModel = mongoose.model("Menue", menueItem)
 
-restrentModel.create({ name: 'the cheeseCake factory ', 
-yelpUrl: 'https://www.yelp.com/biz/the-cheesecake-factory-san-francisco-2?osq=The+Cheesecake+Factory',
-item:[menueItem[0]],
-address:{street:"The North Ring",
-zipcode: 1212 }})
+module.exports = {
+  restrentModel: restrentModel,
+  menuModel: menuModel
+}
+// restrentModel.create({ name: 'the cheeseCake factory ', 
+// yelpUrl: 'https://www.yelp.com/biz/the-cheesecake-factory-san-francisco-2?osq=The+Cheesecake+Factory',
+// item:[menueItem[0]],
+// address:{street:"The North Ring",
+// zipcode: 1212 }})
 
-restrentModel.create({ name: 'Mona Lisa ', 
-yelpUrl: 'https://www.yelp.com/biz/mona-lisa-restaurant-san-francisco',
-item:[menueItem[0]],
-address:{street:"Columbus Ave",
-zipcode: 1232} })
+// restrentModel.create({ name: 'Mona Lisa ', 
+// yelpUrl: 'https://www.yelp.com/biz/mona-lisa-restaurant-san-francisco',
+// item:[menueItem[0]],
+// address:{street:"Columbus Ave",
+// zipcode: 1232} })
 
-restrentModel.create({ name: 'Fog Harbor Fish House ', 
-yelpUrl: 'https://www.yelp.com/biz/fog-harbor-fish-house-san-francisco-2',
-item:[menueItem[0]],
-address:{street:"Pier",
-zipcode: 1312} })
+// restrentModel.create({ name: 'Fog Harbor Fish House ', 
+// yelpUrl: 'https://www.yelp.com/biz/fog-harbor-fish-house-san-francisco-2',
+// item:[menueItem[0]],
+// address:{street:"Pier",
+// zipcode: 1312} })
 
-menuModel.create({ title:'apple' },{title:'orange' })
+// menuModel.create({ title:'apple' },{title:'orange' })
 
-  .then((restrunt) => {
-    console.log(restrunt);
+//   .then((restrunt) => {
+//     console.log(restrunt);
+//   })
+//   .then((menue) => {
+//     console.log(menue);
+//   })
+//   .catch((error) => {
+//     console.log(error)
+//   })
+
+//   restrentModel.findOne({name: the }, callback)
+
+
+// router.get('/restrunts', (request, response) => {
+//     restrentModel.find({})
+//       .then((restrunts) => {
+//         response.send({ 
+//             restrunts: restrunts    § 
+//         })
+//       })
+//       .catch((error) => {
+//         console.log(error)
+//       })
+//   })
+const update = (id,field,value) =>{
+ restrentModel.findById(id)
+  .then(rest => { 
+      console.log(rest[field])
+      rest[field] = value
+      rest.save()
   })
-  .then((menue) => {
-    console.log(menue);
-  })
-  .catch((error) => {
-    console.log(error)
-  })
+  .then(console.error)
+  .then(done)
+}
 
-  restrentModel.findOne({name: the }, callback)
+update('5e262b84b1b5204c7ba5696d', 'name','Mona Lisa')
 
-// db.restrunts.find({name: /the/ }).pretty()
-// db.restrunts.find({ address: { zipcode: /1212/ } }).pretty()
-//
 
-router.get('/restrunts', (request, response) => {
-    restrentModel.find({})
-      .then((restrunts) => {
-        response.send({ 
-            restrunts: restrunts
-        })
-      })
-      .catch((error) => {
-        console.log(error)
-      })
+const destroy = (id) => {
+  restrentModel.findById(id)
+  .then(rest => {
+      rest.remove()
   })
-  
+  .catch(console.error)
+  .then(done)
+
+}
+destroy('5e262b84b1b5204c7ba5696d')
--- db/seed.js
+++ db/seed.js
@@ -1,1 +1,75 @@
-let Schema = require("../db/schema.js");
+let mongoose = require('mongoose');
+let Schema = require("./schema.js");
+let db = mongoose.connection
+
+let restrentModel = Schema.restrentModel;
+let menuModel = Schema.menuModel;
+
+// First we clear the database of existing students and projects.
+restrentModel.remove({})
+  .then(() => {
+    console.log('All rest deleted!')
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+  menuModel.remove({})
+  .then(() => {
+    console.log('All items deleted!')
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+
+
+
+  const dinner = new menuModel({ title:'apple' })
+const launch = new menuModel({title:'orange' })
+const drinks = new menuModel({title:'tea' })
+const drink = new menuModel({title:'coffee' })
+
+const menueItems = [dinner, launch, drinks, drink];
+
+
+menuModel.insertMany(menueItems)
+  .then(() => {
+    console.log(`Added ${menueItems.length} items to database.`)
+   // console.log(menueItems)
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+
+  const rest1 = new restrentModel({ name: 'the cheeseCake factory ', 
+yelpUrl: 'https://www.yelp.com/biz/the-cheesecake-factory-san-francisco-2?osq=The+Cheesecake+Factory',
+item:[menueItems[0]],
+address:{street:"The North Ring",
+zipcode: 1212 }})
+
+const rest2 = new restrentModel({name: 'Mona Lisa ', 
+yelpUrl: 'https://www.yelp.com/biz/mona-lisa-restaurant-san-francisco',
+item:[menueItems],
+address:{street:"Columbus Ave",
+zipcode: 1232}})
+
+const rest3 = new restrentModel({name: 'Fog Harbor Fish House ', 
+yelpUrl: 'https://www.yelp.com/biz/fog-harbor-fish-house-san-francisco-2',
+item:[menueItems],
+address:{street:"Pier",
+zipcode: 1312} })
+
+const rests = [rest1, rest2, rest3 ]
+
+restrentModel.insertMany(rests)
+  .then(() => {
+    console.log(`Added ${rests.length} restrunts to database.`)
+    //console.log(rests)
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+  .then(()=> db.close())
+
+  //db.rests.find({name: /the/ }).pretty()
+//db.restrunts.find({ address: { zipcode: /1212/ } }).pretty()
+

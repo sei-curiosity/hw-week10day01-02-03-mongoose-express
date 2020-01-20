2d58dd7425cd0d8568cc69d78d2f88761c06bd14
lmdsultan <mdgetbetter.gmail.com>
2020-01-20 21:16:37 +0300

    1 and 2 W10

--- db/schema.js
+++ db/schema.js
@@ -1,4 +1,24 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/yum');
+mongoose.connect('mongodb://localhost/yum',
+{useNewUrlParser: true , useUnifiedTopology: true })
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
+db.on('error', console.error.bind(console, 'connection error:'))
+
+let Schema = mongoose.Schema
+let MenuItemsSchema = new Schema ({
+    title: String
+})
+let RestaurantSchema = new Schema ({
+    name: String,
+    address: - {street: String, zipcode: Number},
+    yelpUrl: String,
+    items: [MenuItemsSchema]
+})
+
+let MenuItemsModel = mongoose.model("Menu", MenuItemsSchema)
+let RestaurantModel = mongoose.model("Restaurant", RestaurantSchema)
+
+module.exports = {
+    MenuItemsModel: MenuItemsModel,
+    RestaurantModel: RestaurantModel
+}
--- db/seed.js
+++ db/seed.js
@@ -1,1 +1,40 @@
 let Schema = require("../db/schema.js");
+
+const MenuItemsModel = Schema.MenuItemsModel
+let RestaurantModel = Schema.RestaurantModel
+
+const stitem = new MenuItemsModel({title:"Kebsa"})
+const scitem = new MenuItemsModel({title:"Mendi"})
+const thitem = new MenuItemsModel({title:"Jresh"})
+const fritem = new MenuItemsModel({title:"Burger"})
+const fvitem = new MenuItemsModel({title:"Fries"})
+
+const stresaurant = new RestaurantModel ({
+    name: "Romancia",
+    address: {street: "King Fahad", zipcode: 14244},
+    yelpUrl: "www.R.com",
+    items: [stitem,thitem]
+})
+
+const scresaurant = new RestaurantModel ({
+    name: "Yeman",
+    address: {street: "Saba", zipcode: 14234},
+    yelpUrl: "www.Y.com",
+    items: [stitem,thitem,scitem]
+})
+
+const thresaurant = new RestaurantModel ({
+    name: "Wow ME",
+    address: {street: "Blvd", zipcode: 14241},
+    yelpUrl: "www.W.com",
+    items: [fritem,fvitem]
+})
+
+const allrestaurant = [stresaurant,scresaurant,thresaurant]
+RestaurantModel.insertMany(allrestaurant)
+.then(()=>{
+    console.log(`Restaurant added ${allrestaurant.length} to database`)
+})
+.catch((error) => {
+    console.log(error)
+})

26f89a3f4ed7e09dfa4b5fd77ba61064549d0fc2
Abdulah68 <abdulahali6565@gmail.com>
2020-01-20 06:59:56 +0300

    solved hw-week10day01

--- db/schema.js
+++ db/schema.js
@@ -2,3 +2,29 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/yum');
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open', function() {
+    console.log('database successfully connected')
+  })
+
+  let Schema = mongoose.Schema // this is a schema method
+
+  let MenuItemsSchema = new Schema ({
+    title: String
+})
+
+  let RestaurantSchema = new Schema ({
+    name: String,
+    address:{
+        street: String,
+        zipcode: Number
+    },
+    yelpUrl: String,
+    items: [MenuItems]
+})
+
+let RestaurantModel =mongoose.model("Restaurant", RestaurantSchema)
+
+let MenuItemsModel =mongoose.model("MenuItem", MenuItemsSchema)
+
+
--- db/seed.js
+++ db/seed.js
@@ -1,1 +1,29 @@
 let Schema = require("../db/schema.js");
+
+let RestaurantModel = Schema.RestaurantModel ;
+
+let MenuItemsModel= Schema.MenuItemsModel;
+
+RestaurantModel.remove({})
+  .then(() => {
+    console.log('All Restaurant deleted!')
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+
+  MenuItemsModel.remove({})
+  .then(() => {
+    console.log('All MenuItems deleted!')
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+  
+
+const Restaurant1 = new RestaurantModel({name:"Pizza Hut",address:{ street:" road king fahad" ,  zipcode:"123"}, yelpUrl:"https://www.pizzahut.com.sa/" })
+const Restaurant2 = new RestaurantModel({name:"KFC",address:{ street:" road king abdullh" ,  zipcode:"1234"}, yelpUrl:"https://www.kfc.com/" })
+
+
+const projects = [Restaurant1, Restaurant2, ]
+

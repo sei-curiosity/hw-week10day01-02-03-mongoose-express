ae47bf6c9eca67f687f9bfdd84c490461dafcfb5
NooraAbdullah <noorasubaiee@gmail.com>
2020-01-22 09:04:04 +0300

    Hw

--- db/schema.js
+++ db/schema.js
@@ -2,3 +2,30 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/yum');
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
+
+
+//Schemas
+
+const RestaurantSchema = new mongoose.Schema({
+    name: String,
+    address: {
+        street: String, 
+        zipcode: Number
+    },
+    yelpUrl: String,
+    items: [MenuItemsSchema]
+})
+
+const MenuSchema = new mongoose.Schema({
+    title: String
+})
+
+//models
+
+const MenuModel = mongoose.model("Menu",MenuSchema)
+const RestaurantModel = mongoose.model("Restaurant",RestaurantSchema)
+
+module.exports = {
+    RestaurantModel: RestaurantModel,
+    MenuModel: MenuModel
+}
--- db/seed.js
+++ db/seed.js
@@ -1,1 +1,63 @@
+// let mongoose = require("mongoose")
 let Schema = require("../db/schema.js");
+let db = mongoose.connection
+let RestaurantModel = Schema.RestauranModel
+let MenueItemsModel = Schema.MenueModel
+
+
+const menue1 = new MenueItemsModel ({
+    title:"Alfredo pasta"
+})
+const menue2 = new MenueItemsModel ({
+    title:"Fettuccine pasta"
+})
+const menue3 = new MenueItemsModel ({
+    title:"Masala pasta"
+})
+
+const menues = [menue1, menue2, menue3]
+///
+
+const restaurant1 = new RestauranModel({
+    name: "Pasta Factory",
+    address:{
+        street: "xx",
+        zipcode: 444
+    },
+    yelpUrl:"xxx",
+    items: menues
+})
+
+const restaurant2 = new RestauranModel({
+    name: "Noodles Factory",
+    address:{
+        street: "xx",
+        zipcode: 444
+    },
+    yelpUrl:"xxx",
+    items: menues
+})
+
+const restaurant3 = new RestauranModel({
+    name: "Applebees",
+    address:{
+        street: "yy",
+        zipcode: 555
+    },
+    yelpUrl:"yyy",
+    items: menues
+})
+
+const restaurants = [restaurant1,restaurant2,restaurant3]
+
+
+ResturantModel.insertMany(restaurants)
+  .then(() => {
+    console.log(`Added ${restaurants.length} restaurants to database`)
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+  .then(()=> db.close())
+
+//   exports

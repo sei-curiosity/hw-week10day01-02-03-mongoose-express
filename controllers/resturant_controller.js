dd78d3a0c32df80e4f9e7432a13320bc4e9a492b
Mashael <mashael@Mashaels-MacBook-Pro.local>
2020-01-22 13:04:45 +0300

    solution

--- controllers/resturant_controller.js
+++ controllers/resturant_controller.js
@@ -4,7 +4,7 @@ const Schema = require("../db/schema.js")
 const resturant = Schema.resturantModel
 const menu = Schema.menuModel
 
-router.get('/resturant',(req,res)=> {
+router.get('/resturants',(req,res)=> {
     resturant.find({})
     .then((resturants)=> {
         res.send({
@@ -17,6 +17,45 @@ router.get('/resturant',(req,res)=> {
     )
 })
 // index()
+router.get('/resturants/:id',(req,res) => {
+resturant.findById(req.params.id) 
+    .then(resturant => {
+        res.send(resturant)
+    })
+    .catch (
+        error => console.error(error)
+    )
+})
+//show() show function find by id
 
+router.patch('/resturants/:id',(req,res)=> {
+    resturant.findByIdAndUpdate(req.params.id)
+    .then(resturant=>{
+        res.send(resturant)
+    })
+    .catch (
+        error => console.error(error)
+    )
+})
+router.delete('/resturants/:id',(req,res)=> {
+    resturant.findByIdAndDelete(req.params.id)
+    .then(resturant=>{
+        res.send()
+    })
+    .catch(
+        error=> console.error(error)
+    )
+})
+
+router.post('/resturants', (req, res) => {
+let resBody = {name:'firegrill', address:{street:'alsulaymaniah', zipcode:'335'},yelUrl:'www.firegrill.com'}
+resturant.create(resBody)
+.then(resturant=> {
+    res.send(" new resturant is created")
+})
+.catch(
+    error=> console.error(error)
+)
+  });
 
 module.exports = router

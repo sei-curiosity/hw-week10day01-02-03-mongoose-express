b8ef30b5367570c6dc0ff29e98e8f4bc96633ee6
gneral20 <gneral20@gmail.com>
2020-01-26 18:51:24 +0300

    the saving models fixed

--- db/seed.js
+++ db/seed.js
@@ -5,17 +5,33 @@ let resturantModel = Schema.resturantModel;
 let menuItemModel = Schema.menuItemModel;
 
 
-let Menu1 = new menuItemModel({title: "Kabsa"})
-let Menu2 = new menuItemModel({title: "Shawrma"})
-let Menu3 = new menuItemModel({title: "Nodels"})
+let Menu1 = new menuItemModel({
+    title: "Kabsa"
+})
+Menu1.save()
+let Menu2 = new menuItemModel({
+    title: "Shawrma"
+})
+Menu2.save()
+let Menu3 = new menuItemModel({
+    title: "Nodels"
+})
+Menu3.save()
+
 
 let menu = [Menu1, Menu2, Menu3]
 
 let MeshalResturant = new resturantModel({
     name: "Meshal Resturant",
-    address: {street: "al-mothalth", zipcode: 1121},
+    address: {
+        street: "al-mothalth",
+        zipcode: 1121
+    },
     yelpUrl: "//https:www.google.com",
     items: menu
 })
+MeshalResturant.save()
 
-MeshalResturant.find({name: "Meshal Resturant"})
+MeshalResturant.find({
+    name: "Meshal Resturant"
+})

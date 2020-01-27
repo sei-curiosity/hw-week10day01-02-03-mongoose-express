8e5159de8efe58685735d4d625eca27bc97ff17e
maysa9 <mayar.a.saleh@outlook.com>
2020-01-27 08:53:12 +0300

    finally hw

--- db/schema.js
+++ db/schema.js
@@ -1,4 +1,12 @@
-const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/yum');
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
+db.on('error', console.error(console,'error'));
+let Schema = mongoose.Schema
+let MenuSchema = new Schema({
+    title: String
+})
+let RestaurantSchema = new Schema({ name: String,address: {street:String, zipcode:Number},yelpUrl: String, items: [MenuSchema]})
+let MenuModel = mongoose.model("Menu",MenuSchema)
+let RestaurantModel = mongoose.model("Restaurant",RestaurantSchema)
+
+module.exports = { RestaurantModel: RestaurantModel, MenuModel: MenuModel}
--- db/seed.js
+++ db/seed.js
@@ -1,1 +1,24 @@
 let Schema = require("../db/schema.js");
+let Schema = require("../db/schema.js");
+
+let RestaurantModel = Schema.RestaurantModel
+let MenuModel = Schema.MenuModel
+const menu1=new MenuModel({title:"superfood"})
+const menu2=new MenuModel({title:"Sandwiches "})
+const menu3=new MenuModel({title:"briskett"})
+const menu4 =new MenuModel({title:"goulash soup"})
+
+const restaurant2=new RestaurantModel({
+    name:"o",address:{street:"ooo",zipcode:999},yelpUrl:"https://yelp/oooo",items:[menu1,menu2,menu3]})
+const restaurant2=new RestaurantModel({
+    name:"k",address:{street:"kkkk",zipcode:999},yelpUrl:"https://yelp/kkkkk",items:[menu1,menu2,menu3]})
+const restaurant2=new RestaurantModel({
+    name:"i",address:{street:"iiii",zipcode:999},yelpUrl:"https://yelp/iiiiiiiiii",items:[menu1,menu2,menu3]})
+    const restaurants=[restaurant1,restaurant2,restaurant3]
+    RestaurantModel.insertMany(restaurants)
+    .then(()=>{
+        console.log(`Add ${restaurants.length} restuant`)
+    })
+    .catch((error)=>{
+        console.log(error)
+    })

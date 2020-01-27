5b79dec68846a9a809921dd44c35ae97a7de67a1
monano <mona.gaihab@gmail.com>
2020-01-27 08:52:59 +0300

    done all parts

--- routes/restaurant_routes.js
+++ routes/restaurant_routes.js
@@ -3,16 +3,17 @@ const express = require("express")
 //import needed models
 const Schema = require('../models/schema')
 const Restaurant = Schema.RestaurantModel
-// route
-const route = express.Router()
-
-//root route
-route.get("/",(req,res)=>{
-   res.render("layout")
+const Menu = Schema.MenuModel
+// router
+const router = express.Router()
+// cors//midleware //npm install cors //.use(localhost:9000)//then connected with react
+//root router
+router.get("/",(req,res)=>{
+   res.send("Root Path")
 })
 //index 
 //GET /restaurants
-route.get("/restaurants",(req,res)=>{
+router.get("/restaurants",(req,res)=>{
     Restaurant.find()
     .then(restaurants =>{
         res.status(200).json({restaurants: restaurants})
@@ -22,8 +23,8 @@ route.get("/restaurants",(req,res)=>{
 
 //create
 //POST /restaurants
-route.post("/restaurants",(req,res)=>{
-    const newRestaurants= req.body.restaurant;
+router.post("/restaurants",(req,res)=>{
+    const newRestaurants= req.body;
     Restaurant.create(newRestaurants)
     .then(restaurant =>{
         res.status(201).json({restaurant:restaurant})
@@ -33,7 +34,7 @@ route.post("/restaurants",(req,res)=>{
 
 //show
 //GET /restaurants/:id
-route.get("/restaurants/:id",(req,res)=>{
+router.get("/restaurants/:id",(req,res)=>{
     const restaurantId=req.params.id
     Restaurant.findById(restaurantId)
     .then(restaurant =>{
@@ -44,8 +45,8 @@ route.get("/restaurants/:id",(req,res)=>{
 
 //update
 //PATCH /restaurants/:id
-route.patch("/restaurants/:id",(req,res)=>{
-    const restaurantBody = req.body.restaurant
+router.patch("/restaurants/:id",(req,res)=>{
+    const restaurantBody = req.body
     const restaurantId = req.params.id
     Restaurant.findById(restaurantId)
     .then(restaurant => {
@@ -57,7 +58,7 @@ route.patch("/restaurants/:id",(req,res)=>{
 
 //distroy
 //DELETE /restaurants/:id
-route.delete("/restaurants/:id",(req,res)=>{
+router.delete("/restaurants/:id",(req,res)=>{
     const restaurantId = req.params.id
     Restaurant.findById(restaurantId)
     .then(restaurant =>{
@@ -67,4 +68,78 @@ route.delete("/restaurants/:id",(req,res)=>{
     .catch(console.error)
 })
 
-module.exports=route
+
+router.get('/restaurants/:restaurantid/menus', (req, res) => {
+    const restaurantid = req.params.restaurantid
+    Restaurant.findById(restaurantid)
+    .then((restaurant) => {
+        res.send({
+            menus: restaurant.items
+        })
+    })
+})
+
+// show a specific menu by a specific restaurant
+router.get("/restaurants/:restaurantId/menus/:menuId",(req,res)=>{
+    const restaurantId=req.params.restaurantId
+    const menuId= req.params.menuId
+    Restaurant.findById(restaurantId)
+    .then((restaurant)=>{
+        const currentMenu = restaurant.items.find(menu=>menu._id==menuId)
+        res.send({
+            menu:currentMenu
+        })
+    })
+})
+
+//! Create a new menu for a specific restaurant
+router.post('/restaurants/:restaurantid', (req, res)=> {
+    const restaurantid = req.params.restaurantid
+    const newMenu = req.body
+    const createMenu = new Menu ({"title": newMenu.title})
+    Restaurant.findById(restaurantid)
+    .then((restaurant)=> {//here we push the menu in restaurant menus
+        restaurant.items.push(createMenu)
+        return restaurant // after updating the restaurant we want to return it to save it in the athor then statment
+    })
+    .then (restaurant => restaurant.save()) // here we save the menu to the restaurant
+    .then((savedrestaurantWithNewMenu)=> {
+        res.redirect(`/restaurants/${restaurantid}`)
+    })
+    .catch(err => console.log(err))
+    })
+
+//! update a specific menu by a specific restaurant
+router.put('/restaurants/:restaurantid/menus/:menuid', (req, res)=> {
+    const restaurantid = req.params.restaurantid
+    const menuid = req.params.menuid
+    const updatedMenu = req.body
+    Restaurant.findById(restaurantid)
+    .then((restaurant)=> {
+        let index = restaurant.items.findIndex(x => x._id == menuid)
+        restaurant.items[index].title = updatedMenu.title
+        return restaurant
+    })
+    .then((restaurant) => restaurant.save())
+    .then((restaurant) => res.redirect(`/restaurants/${restaurantid}/menus/${menuid}`))
+    .catch(err => console.log(err))
+})
+
+//!delete a specific menu by a specific restaurant
+router.delete('/restaurants/:restaurantid/menus/:menuid', (req, res) => {
+    const restaurantid = req.params.restaurantid
+    const menuid = req.params.menuid
+    Restaurant.findById(restaurantid)
+    .then ((restaurant) => {
+        let index = restaurant.items.findIndex(x => x._id == menuid)
+        if(index > -1) {
+            restaurant.items.splice(index, 1)
+        }
+        return restaurant
+    })
+    .then(restaurant => restaurant.save())
+    .then(()=> res.redirect(`/restaurants/${restaurantid}/menus`))
+    .catch(err => console.log(err))
+})
+
+module.exports=router
